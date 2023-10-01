844fd847250fbd22c6e5e2fafb1c3f1e07f02a7c
KaushikDas9 <dasbabai2017@gmail.com>
2023-10-01 19:24:36 +0530

    upload

--- lib/WebViewPage.dart
+++ lib/WebViewPage.dart
@@ -11,6 +11,9 @@ class webViewDesign extends StatefulWidget {
 
 class _webViewDesignState extends State<webViewDesign> {
   late String a;
+  InAppWebViewController? webViewController;
+
+  TextEditingController? controller;
 
   @override
   void initState() {
@@ -26,9 +29,11 @@ class _webViewDesignState extends State<webViewDesign> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        centerTitle: true,
         title: Text("News app"),
       ),
       body: InAppWebView(
+        onWebViewCreated: (controller) => {webViewController = controller},
         initialUrlRequest: URLRequest(url: Uri.parse(a)),
       ),
     );
