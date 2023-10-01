1a90a26bb8ae168f81d7e9cb81e1ab232abca8a0
KaushikDas9 <dasbabai2017@gmail.com>
2023-10-01 20:59:08 +0530

    upload

--- lib/WebViewPage.dart
+++ lib/WebViewPage.dart
@@ -12,8 +12,9 @@ class webViewDesign extends StatefulWidget {
 class _webViewDesignState extends State<webViewDesign> {
   late String a;
   InAppWebViewController? webViewController;
-
+  bool isloading = false;
   TextEditingController? controller;
+  PullToRefreshController? refreshController;
 
   @override
   void initState() {
@@ -31,11 +32,42 @@ class _webViewDesignState extends State<webViewDesign> {
       appBar: AppBar(
         centerTitle: true,
         title: Text("News app"),
+        leading: InkWell(
+            child: Icon(Icons.arrow_back_ios_new_outlined),
+            onTap: () {
+              webViewController!.goBack();
+            }),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh_rounded),
+            onPressed: () {
+              print("reload");
+              webViewController?.reload();
+            },
+          ),
+          Padding(padding: EdgeInsets.only(right: 5))
+        ],
       ),
-      body: InAppWebView(
-        onWebViewCreated: (controller) => {webViewController = controller},
-        initialUrlRequest: URLRequest(url: Uri.parse(a)),
-      ),
+      body: Stack(alignment: Alignment.center, children: [
+        InAppWebView(
+          onPageCommitVisible: (controller, url) {
+            setState(() {
+              isloading = false;
+            });
+          },
+          onWebViewCreated: (controller) {
+            webViewController = controller;
+            setState(() {
+              isloading = true;
+            });
+          },
+          initialUrlRequest: URLRequest(url: Uri.parse(a)),
+        ),
+        Visibility(
+          child: CircularProgressIndicator(),
+          visible: isloading,
+        )
+      ]),
     );
   }
 }
